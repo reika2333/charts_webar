32dd40413748e05c189ac2a7bf866b53ee2d8faf
刘岭柯 <reika2333@yahoo.co.jp>
2019-08-18 22:58:05 +0800

    llk
    llk create homepage

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import bgImg from "./bg-img.jpg"
+import './homepage.css';
+
+export default () => (
+  <div id="Homepage">
+    <h1 className="title-welcome">welcome to </h1>
+    <h2 className="title-name">AR - Big Data Of Medicine</h2>
+    <button className="content-btn">let's scan</button>
+  </div>
+);
+
