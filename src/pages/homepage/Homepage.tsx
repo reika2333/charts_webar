2fbf8de5b4cb2b9a5f935146f83a6b0d6bcd3d0c
刘岭柯 <reika2333@yahoo.co.jp>
2019-09-03 17:12:41 +0800

    [LingKe] add <a> in btn

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -8,14 +8,15 @@ export default () => (
   <div id="Homepage">
     <h1 className="title-welcome">welcome to</h1>
     <h2 className="title-name">AR - Big Data Of Medicine</h2>
-    <Button
-      buttonStyle={{ marginTop: '250px' }}
-      onClick={() => {
-        console.log('Button Clicked!');
-        window.location.href = './cube.html';
-      }}
-    >
-      let's scan
+    <Button buttonStyle={{ marginTop: '250px' }}>
+      <a
+        href="./cube.html"
+        onClick={() => {
+          console.log('Button Clicked!');
+        }}
+      >
+        let's scan
+      </a>
     </Button>
     {/* <Cube /> */}
   </div>
