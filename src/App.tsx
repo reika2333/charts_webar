152364b2645dc8c6c69c3eb25f51e0f054984e76
soonberry <shuwenli89@126.com>
2019-09-04 15:46:20 +0800

    [Shuwen] fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
 import ARPage from './pages/arpage/ARPage';
 
