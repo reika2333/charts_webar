8af86acc5721eef35d5fd67b7b707162afd227b9
刘岭柯 <reika2333@yahoo.co.jp>
2019-09-07 11:42:14 +0800

    [LingKe] change opacity and position of box

--- src/pages/arpage/cube.ts
+++ src/pages/arpage/cube.ts
@@ -2,7 +2,18 @@ const cubeHtml: string = `
     <a-scene embedded arjs="debugUIEnabled:false;">
 
         <a-marker preset="hiro">
-          <a-box position="0 0 0"></a-box>
+
+          <a-box position="0 0.5 0"
+                height="1"
+                width="0.4"
+                depth="0.4"
+                material="color: white;side: double;opacity: 0.5"></a-box>
+
+          <!--坐标-->
+          <a-entity line="start: 0, 0, 0; end: 0 0 10; color: red"
+          line__2="start: 0, 0, 0; end: 0 10 0; color: green"
+          line__3="start: 0, 0, 0; end: 10 0 0; color: blue"></a-entity>
+
         </a-marker>
   
         <a-entity camera></a-entity>
